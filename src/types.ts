3a4cc94a84f78f86d120591039edcf0ef091c046
孔乙己拉夫米 <zhangzhengzeng131@qq.com>
2024-03-13 10:38:51 +0800

    象棋类型完善

--- src/types.ts
+++ src/types.ts
@@ -5,8 +5,17 @@ import { PieceList, Piece } from "./piece"
  */
 export type PieceSide = "RED" | "BLACK"
 
+/**
+ * 棋子坐标点
+ */
 export interface PiecePositonPoint {
+  /**
+   * x轴位置
+   */
   x: number
+  /**
+   * y轴位置
+   */
   y: number
 }
 
@@ -15,14 +24,25 @@ export interface PieceInputInfo extends PiecePositonPoint, PieceMethods {
   side: PieceSide
   isChoose: boolean
   isLastMove: boolean
+  isGeneral:boolean
 }
 
-
+/**
+ * 棋子具有的方法
+ */
 export interface PieceMethods {
-  draw: (this: Piece, startX: number, startY: number, endX: number, endY: number, ctx?: CanvasRenderingContext2D) => void
+  /**
+   * 棋子绘画
+   * @param this 棋子本身
+   * @param x x坐标点
+   * @param y y坐标点
+   * @param radius 半径
+   * @param ctx canvas绘画
+   * @returns 
+   */
+  draw: (this: Piece, x: number, y: number, radius: number, ctx?: CanvasRenderingContext2D) => void
   move: (this: Piece, pos: MovePoint | PiecePositonPoint, PieceList: PieceList) => MoveResult
   getMovePointList: (this: Piece, pl: PieceList) => MovePointList
-  drawMovePointList: (this: Piece, pl: PieceList, startX: number, startY: number, width: number, height: number, radius: number, color: string, ctx: CanvasRenderingContext2D) => void
 }
 
 
@@ -33,3 +53,16 @@ export type MovePoint = PiecePositonPoint & {
 
 export type MovePointList = MovePoint[]
 export type MoveResult = { flag: true } | { flag: false, message: string }
+
+export interface BoardInfo {
+  width: number
+  height: number
+  padding: number
+  viewSide: PieceSide
+  scaleRatio?: number
+  context?: CanvasRenderingContext2D
+}
+
+export type BoardMatrix = Array<Array<null | Piece>>
+
+export type MoveType = "MOVE" | "EAT" | "kINGUNDERATTACK"
